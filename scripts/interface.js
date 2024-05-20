60806d3f9320b82e825f1330f8450761dec4ede9
robertomoreira04 <robertodev04@gmail.com>
2024-05-20 15:44:22 -0300

    Jogo da velha

---
+++ scripts/interface.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
+    });
+});
+
+function handleClick(event) {
+    let square = event.target;
+    let position = square.id;
+
+    if (handleMove(position)) {
+        setTimeout(() => {
+            alert("O jogo acabou!");
+        }, 10);
+    }
+
+    updateSquares();
+};
+
+function updateSquares() {
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        let position = square.id;
+        let symbol = board[position];
+
+        if (symbol !== '') {
+            square.innerHTML = `<div class='${symbol}'>${symbol}</div>`;
+        }
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
